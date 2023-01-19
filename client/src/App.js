e1ba91a69b7dafc8abc35219e442f649811a7501
Ryland Adams <rylandadams@yahoo.com>
2023-01-19 12:49:35 -0800

    styled component and client using themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,40 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import createTheme from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import { themeSettings } from 'theme';
 import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import Layout from 'scenes/layout';
+import Dashboard from 'scenes/dashboard';
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
 	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 	return (
 		<div className='app'>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-			</ThemeProvider>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route element={<Layout />}>
+							<Route
+								path='/'
+								element={
+									<Navigate
+										to='/dashboard'
+										replace
+									/>
+								}
+							/>
+							<Route
+								path='/dashboard'
+								element={<Dashboard />}
+							/>
+						</Route>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+const { styled } = require('@mui/system');
+const { Box } = require('@mui/material');
+
+const FlexBetween = styled(box)({
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+});
+
+export default FlexBetween;
