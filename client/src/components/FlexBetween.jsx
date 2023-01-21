bd61061bbaf47d5dfa043954379356263ef519a3
Ryland Adams <rylandadams@yahoo.com>
2023-01-20 18:57:30 -0800

    nav bar buildout

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,7 +1,7 @@
 const { styled } = require('@mui/system');
 const { Box } = require('@mui/material');
 
-const FlexBetween = styled(box)({
+const FlexBetween = styled(Box)({
 	display: 'flex',
 	justifyContent: 'space-between',
 	alignItems: 'center',
