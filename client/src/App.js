37f0fb79655d3894922fa58e934f07f46eb06406
Ryland Adams <rylandadams@yahoo.com>
2023-01-23 14:33:32 -0800

    customers data grid front and backend functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from 'scenes/layout';
 import Dashboard from 'scenes/dashboard';
 import Products from 'scenes/products';
+import Customers from 'scenes/customers';
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
@@ -35,6 +36,10 @@ function App() {
 							<Route
 								path='/products'
 								element={<Products />}
+							/>
+							<Route
+								path='/customers'
+								element={<Customers />}
 							/>
 						</Route>
 					</Routes>
