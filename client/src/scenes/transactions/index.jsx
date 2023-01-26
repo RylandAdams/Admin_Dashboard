d62ef4166820ea6130e468143ea171db3854aa26
Ryland Adams <rylandadams@yahoo.com>
2023-01-26 13:50:55 -0800

    transactions grid setup

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Box, useTheme } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { useGetTransactionsQuery } from 'state/api';
+import DataGridCustomToolBar from 'components/DataGridCustomToolBar';
 import Header from 'components/Header';
 
 const Transactions = () => {
@@ -12,15 +13,104 @@ const Transactions = () => {
 	const [sort, setSort] = useState({});
 	const [search, setSearch] = useState('');
 
+	const [searchInput, setSearchInput] = useState('');
+
 	const { data, isLoading } = useGetTransactionsQuery({
 		page,
 		pageSize,
 		sort: JSON.stringify(sort),
 		search,
 	});
-	console.log('🚀 ~ file: index.jsx:20 ~ Transactions ~ data', data);
 
-	return <div>Transactions</div>;
+	const columns = [
+		{
+			field: '_id',
+			headerName: 'ID',
+			flex: 1,
+		},
+		{
+			field: 'serId',
+			headerUserIdName: 'User Id',
+			flex: 1,
+		},
+		{
+			field: 'createdAt',
+			headerName: 'CreatedAt',
+			flex: 1,
+		},
+		{
+			field: 'products',
+			headerName: '# of Products',
+			flex: 0.5,
+			sortable: false,
+			renderCell: (params) => params.value.length,
+		},
+		{
+			field: 'cost',
+			headerName: 'Cost',
+			flex: 1,
+			renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+		},
+	];
+
+	return (
+		<Box m='1.5rem 2.5rem'>
+			<Header
+				title='TRANSACTIONS'
+				subtitle='Entire list of transactions'
+			/>
+			<Box
+				height='80vh'
+				sx={{
+					'& .MuiDataGrid-root': {
+						border: 'none',
+					},
+					'& .MuiDataGrid-cell': {
+						borderBottom: 'none',
+					},
+					'& .MuiDataGrid-columnHeaders': {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						boarderBottom: 'none',
+					},
+					'& .MuiDataGrid-virtualScroller': {
+						backgroundColor: theme.palette.primary.light,
+					},
+					'& .MuiDataGrid-footerContainer': {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						boarderTop: 'none',
+					},
+					'& .MuiDataGrid-toolbarContainer .muiButton-text': {
+						color: `${theme.palette.secondary[200]} !important`,
+					},
+				}}
+			>
+				<DataGrid
+					loading={isLoading || !data}
+					getRowId={(row) => row._id}
+					rows={(data && data.transactions) || []}
+					columns={columns}
+					rowCount={(data && data.total) || 0}
+					rowsPerPageOptions={[20, 50, 100]}
+					pagination
+					page={page}
+					pageSize={pageSize}
+					paginationMode='server'
+					sortingMode='server'
+					onPageChange={(newPage) => setPage(newPage)}
+					onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+					onSortModelChange={(newSortModel) =>
+						setSort(...newSortModel)
+					}
+					components={{ Toolbar: DataGridCustomToolBar }}
+					componentsProps={{
+						toolbar: { searchInput, setSearchInput, setSearch },
+					}}
+				/>
+			</Box>
+		</Box>
+	);
 };
 
 export default Transactions;
