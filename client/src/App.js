de584c0696ccd44e10e4750874a3c0cbf3c80f12
Ryland Adams <rylandadams@yahoo.com>
2023-02-01 14:18:52 -0800

    breakdown page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Geography from 'scenes/geography';
 import Overview from 'scenes/overview';
 import Daily from 'scenes/daily';
 import Monthly from 'scenes/monthly';
+import Breakdown from 'scenes/breakdown';
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
@@ -65,6 +66,10 @@ function App() {
 							<Route
 								path='/monthly'
 								element={<Monthly />}
+							/>
+							<Route
+								path='/breakdown'
+								element={<Breakdown />}
 							/>
 						</Route>
 					</Routes>
